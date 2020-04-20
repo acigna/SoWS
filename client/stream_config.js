d233e99f1378654ac1ddedf1ae1370c45d27007d
Tarik Zakaria Benmerar <tarik.benmerar@acigna.com>
2020-04-20 20:49:03 +0100

    Added client library, with its example and default config for test.

---
+++ client/stream_config.js
@@ -0,0 +1,14 @@
+var stream_config = {
+	ws_url: "ws://localhost:8080/ws",
+	realm: "remote_stream",
+	local_mode: true,
+	stream_url: "rtmp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov",
+	nb_samples: 20,
+}
+
+
+try {
+	module.exports = stream_config;
+} catch(e) {
+
+}
